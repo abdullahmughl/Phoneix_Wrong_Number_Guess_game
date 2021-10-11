126774614175d911d6686e9407271ba43628c689
Abdullah Mughal <abdullahmughal142@gmail.com>
2021-10-11 06:30:16 +0000

    build upload to github

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,7 @@
-#---
-# Excerpted from "Programming Phoenix LiveView",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/liveview for more book information.
-#---
+#--
+#Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like
+#--
 use Mix.Config
 
 # For production, don't forget to configure the url host
